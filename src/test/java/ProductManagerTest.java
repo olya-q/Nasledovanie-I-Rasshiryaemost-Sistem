93e9198b9fdad87e28738e4f9782490424553f80
olya alyakina <olgast97@gmail.com>
2023-03-10 10:15:35 -0500

    Added Test

--- src/test/java/ProductManagerTest.java
+++ src/test/java/ProductManagerTest.java
@@ -18,7 +18,7 @@ class ProductManagerTest {
         Product[] actual = repo.findAll();
         Product[] expected = {book1, book2, book3};
 
-        Assertions.assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, actual);
     }
     @Test
     public void TheSameName() {
@@ -34,6 +34,70 @@ class ProductManagerTest {
         Product[] actual = manager.searchBy("Book");
         Product[] expected = {book1, book2};
 
-        Assertions.assertArrayEquals(expected, actual);
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void testFindOneProduct() {
+        Repository repo = new Repository();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "Book", 200, "Ivanov");
+        Book book2 = new Book(2, "Bookseller", 800, "Bythell");
+        Book book3 = new Book(3, "Slaughterhouse-Five", 200, "Vonnegut");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        Product[] actual = manager.searchBy("Slaughterhouse-Five");
+        Product[] expected = {book3};
+
+        assertArrayEquals(expected, actual);
+    }
+    @Test
+    public void NoOneProductWasFound() {
+        Repository repo = new Repository();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "Book", 200, "Ivanov");
+        Book book2 = new Book(2, "Bookseller", 800, "Bythell");
+        Book book3 = new Book(3, "Slaughterhouse-Five", 200, "Vonnegut");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        boolean actual = manager.matches(book1,"Fo");
+        boolean expected = false;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void ProductWasFound() {
+        Repository repo = new Repository();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "Book", 200, "Ivanov");
+        Book book2 = new Book(2, "Bookseller", 800, "Bythell");
+        Book book3 = new Book(3, "Slaughterhouse-Five", 200, "Vonnegut");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        boolean actual = manager.matches(book1,"Book");
+        boolean expected = true;
+
+        assertEquals(expected, actual);
+    }
+    @Test
+    public void testFindNoOneProduct() {
+        Repository repo = new Repository();
+        ProductManager manager = new ProductManager(repo);
+        Book book1 = new Book(1, "Book", 200, "Ivanov");
+        Book book2 = new Book(2, "Bookseller", 800, "Bythell");
+        Book book3 = new Book(3, "Slaughterhouse-Five", 200, "Vonnegut");
+
+        manager.add(book1);
+        manager.add(book2);
+        manager.add(book3);
+        Product[] actual = manager.searchBy("Fo");
+        Product[] expected = {};
+
+        assertArrayEquals(expected, actual);
     }
 }
